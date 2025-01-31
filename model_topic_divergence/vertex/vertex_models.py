83f08dbaf0dbf593d0614d0d2ac86e9f3fd55080
Adam King <93549638+gg-aking@users.noreply.github.com>
2025-01-31 10:34:48 -0800

    Generalized llm object (#5)
    * moving to vertex folder
    
    * clean up
    
    * better kewargs
    
    * better kewargs
    
    * typo
    
    * rename method
    
    * changing usage to one topic at a time
    
    * changing usage to one topic at a time
    
    * changing usage to one topic at a time
    
    * changing usage to one topic at a time

--- model_topic_divergence/vertex/vertex_models.py
+++ model_topic_divergence/vertex/vertex_models.py
@@ -1,4 +1,4 @@
-import re
+import re, json
 
 from typing import Optional
 
@@ -129,28 +129,34 @@ class VertexTopicDescriber(GeneralVertexModel):
 
         kwargs['vertex_project'] = vertex_project
         if 'instruction' not in kwargs:
-            kwargs['instruction'] = """The following are the names of topics from an NLP topic modeling task. Each topic name may be too specific to generally describe content.
+            kwargs['instruction'] = """You are a data annotation expert, designing a taxonomy of topics for webcontent. 
+            You will be given the name of a topic from an NLP topic modeling task, though the name may be too specific to generally describe content.
 Your task is to generate a topic name that is more general and broadly applicable to the content and to provide a description of content that falls in that topic.
-Example:
- - Topic: "Yahoo Browser Compatibility"
- - Output: "Browser Issues - This topic concerns general issues related to web browsers and ensuring a visitor of a webpage is able to view all content."
+Return the output in a JSON formatted like this: {"topic_name" : ...., "description" : ....}
+Examples:
+ - Input: "Yahoo Browser Compatibility"
+ - Output: {
+            "topic_name" : "Browser Issues",
+            "description" : "Problems users encounter while using web browsers, including slow performance, compatibility errors, or security vulnerabilities, including content that categorizes text that discusses any difficulties related to browser functionality or user experience while browsing the internet.",
+            }
 
- - Topic: "Local Government Finance"
- - Output: "Local Issues - This topic concerns general issues related to a specific location, including local government issues."
-
-Topics:
+ - Input: "Local Government Finance"
+ - Output: {
+            "topic_name" : "Local Issues",
+            "description" : "Encompasses content related to the governance and administrative functions of local authorities, as well as challenges or concerns specific to a local community, including content that addresses municipal policies, public services, community problems, or local governance matters.",
+            }
 """
         if 'vertex_parameters' not in kwargs:
             kwargs['vertex_parameters'] =  {
                         "candidate_count": 1,
-                        "max_output_tokens": 1024,
+                        "max_output_tokens": 256,
                         "temperature": 0,
                         "top_p": 1,
                         "top_k": 1
                 }
         super().__init__(**kwargs)
 
-    def rename_and_describe_topics(self, raw_labels : list[str]) -> list[tuple[str, str]]:
+    def rename_and_describe_topic(self, raw_topic_name : str) -> dict[str, str]:
         """
             Take a list of rough topic names and product a new list of more general topics,
             with short descriptions for each of them.
@@ -161,18 +167,18 @@ Topics:
             tuple[str, str]: A list of pairs of new topic names and label descriptions
         """
 
-        labels_to_rename = ' - ' + '\n - '.join(raw_labels)
-        response = self.send_prompt(text = labels_to_rename)
-        response_text = response.text
-        new_labels = self.parse_and_deduplicate_labels(response_text)
-        return new_labels
-        
-    def parse_and_deduplicate_labels(self, response_text : str) -> list[tuple[str, str]]:
-        new_labels = re.findall(r'\*{0,2}Output:\*{0,2} (.*?) - (.*?)\n', response_text)
+        prompt = self.instruction
+        prompt += f""" - Input: "{raw_topic_name}"
+- Output: """
 
-        new_labels_ = []
-        for i, (name, description) in enumerate(new_labels):
-            if len(new_labels_) == 0 or name not in [name for name, _ in new_labels_]:
-                new_labels_.append((name, description))
-        new_labels = new_labels_.copy()
-        return new_labels
+        response = self.send_prompt(text = prompt, prepend_instruction = False)
+        response_text = response.text
+        j = self.parse_and_deduplicate_label(response_text)
+        return j
+        
+    def parse_and_deduplicate_label(self, response_text : str) -> dict[str, str]:
+        response_text = response_text.replace('\n', ' ')
+        response_text = re.sub(r'^.*\{', '{', response_text)
+        response_text = re.sub(r'\}.*$', '}', response_text)
+        j = json.loads(response_text)
+        return j
